9d40fc25b6948fe7b76cad4f1c29bfc983137de3
Mateusz Pawelec <m.pawelec01@gmail.com>
2024-03-13 14:36:11 +0100

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
