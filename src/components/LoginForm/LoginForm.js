e3b678414245eed561effa3c92de378db9742c19
Ivan Rudenko <rudenko.ivan81@gmail.com>
2024-03-23 15:53:21 +0100

    Styled background + animation
    Finished styles of Loginform, added animation for double cabbage

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,10 +1,13 @@
 import css from "./LoginForm.module.css";
 import React, { useState } from "react";
-import Logo from "../../img/background/logo-kapusta.png";
+import Logo from "./Logo/Logo";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { register, logIn } from "../../redux/auth/operations";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
+import google from "../../img/icon-google.svg";
+import { ReactComponent as GoogleIcon } from '../../img/google-icon.svg';
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -38,49 +41,47 @@ const LoginForm = () => {
     return <Navigate to="/main" />;
   }
   return (
-    <div className={css.container}>
-      {/* <Logo /> */}
-      <div className={css.form_container}>
-        <p>Zaloguj się przy pomocy konta Google</p>
-        <button className={css.google_btn}>
-          <img
-            src="https://e7.pngegg.com/pngimages/326/85/png-clipart-google-logo-google-text-trademark.png"
-            alt="Google"
-          />
-          Google
-        </button>
-        <p>Lub przy pomocy adresu e-mail I hasla, po zarejestrowaniu</p>
-        <form>
-          <label htmlFor="email" className={css.label}>
-            Email:
-          </label>
+    <div className={css.container}>  
+      <div className={css.loginformLogo}>
+        <Logo/>
+      </div>
+      <div className={css.formContainer} >
+        <form className={css.form} >
+          <p className={css.firstText}>
+            You can log in with your Google Account
+          </p>
+          <div className={css.googleLogin}>
+            <Link className={css.google_btn}
+              to={
+                "" //link do google account
+              }
+            >
+              <GoogleIcon/>
+              <p className={css.googleBtnText}>Google</p>
+            </Link>
+          </div>
+          <p className={css.secondText}>Or log in using an email and password, after registering:</p>
+          <label htmlFor="email" className={css.titles}>Email:</label>
           <input
-            className={css.input}
+            className={css.inputs}
             type="email"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="example: user@user.com"
+            placeholder="your@email.com"
             required
           />
-          <label htmlFor="password" className={css.label}>
-            Password
-          </label>
+          <label htmlFor="password" className={css.titles}>Password:</label>
           <input
-            className={css.input}
+            className={css.inputs}
             type="password"
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <div className={css.buttons}>
-            <button className={css.button} onClick={handleLogin}>
-              Login
-            </button>
-            <button className={css.button} onClick={handleRegister}>
-              {" "}
-              Registration{" "}
-            </button>
+            <button onClick={handleLogin} className={css.submitButton}>LOG IN</button>
+            <button onClick={handleRegister} className={css.submitButton}>REGISTRATION</button>
           </div>
         </form>
       </div>
